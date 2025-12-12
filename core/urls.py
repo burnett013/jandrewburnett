2aac3ac755c7913301bd2db102ec01181df87147
Andy Burnett <burnett013@yahoo.com>
2025-12-12 10:13:08 -0600

    Fix template tags by removing whitespace and update UI elements

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('resume/', views.ResumeView.as_view(), name='resume'),
     path('projects/', views.ProjectListView.as_view(), name='project_list'),
-    path('blog/', views.BlogListView.as_view(), name='blog_list'),
+    path('blog/', views.BlogListView.as_view(), name='blog_list'), # List page
 ]
