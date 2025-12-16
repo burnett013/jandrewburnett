54ef5d7b0ebc357047051d648dbadacefd9e860d
Andy Burnett <burnett013@yahoo.com>
2025-12-16 12:57:14 -0600

    Switch to Port 465 (SSL) with Timeout enabled

--- config/settings.py
+++ config/settings.py
@@ -145,9 +145,9 @@ if DEBUG:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    EMAIL_USE_SSL = False
+    EMAIL_PORT = 465
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
     EMAIL_TIMEOUT = 10  # Fail fast (10s) instead of hanging
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
