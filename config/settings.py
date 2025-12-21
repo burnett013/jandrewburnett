9deb42bd2f7ff50e555863c944bb8fa352c3e92a
Andy Burnett <burnett013@yahoo.com>
2025-12-21 10:04:26 -0600

    Explicitly define STATICFILES_DIRS to fix static file 404s

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "core" / "static",
+]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Email Configuration
